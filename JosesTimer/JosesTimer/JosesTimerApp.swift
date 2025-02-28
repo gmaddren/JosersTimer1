b44803d9731fb284b08677b6753ca331f23c12cf
Gary Maddren <gmaddren@gmail.com>
2025-02-28 14:17:36 -0600

    Initial commit from Cursor

---
+++ JosesTimer/JosesTimer/JosesTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  JosesTimerApp.swift
+//  JosesTimer
+//
+//  Created by GARY MADDREN on 2/28/25.
+//
+
+import SwiftUI
+
+@main
+struct JosesTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JosesTimer/JosesTimerTests/JosesTimerTests.swift
@@ -0,0 +1,17 @@
+//
+//  JosesTimerTests.swift
+//  JosesTimerTests
+//
+//  Created by GARY MADDREN on 2/28/25.
+//
+
+import Testing
+@testable import JosesTimer
+
+struct JosesTimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
